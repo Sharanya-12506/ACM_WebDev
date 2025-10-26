41cef80a0f09131179e893f19e117fa2616af13b
Sharanya <sharanyayelluru@gmail.com>
2025-10-27 01:11:11 +0530

    added intro

--- week4/netflix-clone/src/App.js
+++ week4/netflix-clone/src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import MovieRow from "./MovieRow";
 
-function App() {
+function MainApp() {
   const samplePosters = [
-    "https://image.tmdb.org/t/p/w500/8YFL5QQVPy3AgrEQxNYVSgiPEbe.jpg",
-    "https://image.tmdb.org/t/p/w500/6KErczPBROQty7QoIsaa6wJYXZi.jpg",
-    "https://image.tmdb.org/t/p/w500/9O7gLzmreU0nGkIB6K3BsJbzvNv.jpg",
-    "https://image.tmdb.org/t/p/w500/5N20rQURev5CNDcMjHVUZhpoCNC.jpg",
-    "https://image.tmdb.org/t/p/w500/q719jXXEzOoYaps6babgKnONONX.jpg",
+    "/img1.jpeg",
+    "/img2.jpeg",
+    "/img3.jpg",
+    "/img4.png",
+    "/img5.jpg",
+    "/img6.jpeg",
+    "/img7.jpeg",
+    "/img8.jpeg",
+    "/img9.jpeg",
+    "/img12.jpeg",
+    "/img13.jpeg"
   ];
 
   return (
@@ -29,6 +35,7 @@ function App() {
           </ul>
         </nav>
       </header>
+
       <main className="hero">
         <h2>Watch Unlimited Movies & TV Shows</h2>
         <p>Ready to binge?</p>
@@ -49,8 +56,65 @@ function App() {
         </ul>
         <p className="footer-credit">Netflix Clone by Sharanya</p>
       </footer>
-
     </div>
+  );
+}
+
+function App() {
+  const [showIntro, setShowIntro] = useState(true);
+  const [isReady, setIsReady] = useState(false);
+  const videoRef = useRef(null);
+
+  const handleStart = () => {
+    setIsReady(true);
+    videoRef.current.play();
+  };
+
+  return (
+    <>
+      {showIntro ? (
+        <>
+          {!isReady && (
+            <div
+              onClick={handleStart}
+              style={{
+                position: "fixed",
+                inset: 0,
+                background: "black",
+                color: "white",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: "1.5rem",
+                cursor: "pointer",
+                zIndex: 9999,
+              }}
+            >
+              Tap to start
+            </div>
+          )}
+          <video
+            ref={videoRef}
+            src="/netflix-intro.mp4"
+            onEnded={() => setShowIntro(false)}
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              zIndex: 9998,
+              backgroundColor: "black",
+              display: isReady ? "block" : "none",
+            }}
+            controls={false}
+          />
+        </>
+      ) : (
+        <MainApp />
+      )}
+    </>
   );
 }
 
